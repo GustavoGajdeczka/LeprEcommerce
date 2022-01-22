d1cb869d7b3d942479e98cf53e5d6583ceb2c829
Gustavo <tavinhogustavog@gmail.com>
2022-01-21 23:48:54 -0300

    initial commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { AppBar, Toolbar, IconButton, Badge, Menu, MenuItem, Typography } from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+import useStyles from './navbarStyles';
+
+import logo from '../../assets/leprechaun-png-128.png'
+
+const Navbar = () => {
+  const classes = useStyles();
+  return (
+    <>
+    <AppBar position="fixed" className={classes.AppBar} color="inherit">
+      <Toolbar>
+        <Typography variant='h6' className={classes.title} color="inherit">
+          <img 
+            src={logo} 
+            alt="LeprEcommerce" 
+            height="25px" 
+            className='classes.image'/>
+          LeprEcommerce
+        </Typography>
+        <div className={classes.grow}/>
+        <div className="classes.button">
+          <IconButton aria-label="Items no Carrinho" color="inherit">
+            <Badge badgeContent={2} color="secondary">
+              <ShoppingCart/>
+            </Badge>
+          </IconButton>
+        </div>
+      </Toolbar>
+    </AppBar>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Products/prods.js
@@ -0,0 +1,12 @@
+const products = [
+  {"id": 1, "name": "Capacete", "description": "X12", "price": "749.99", "image": "capacete.jpg"},
+  {"id": 2, "name": "Jaqueta", "description": "Deynasa", "price": "499.99", "image": "jaqueta.jpg"},
+  {"id": 3, "name": "Bota", "description": "X12", "price": "349.99", "image": "bota.jpg"},
+  {"id": 4, "name": "Luva", "description": "Tess", "price": "250.00", "image": "luva.jpg"},
+  {"id": 5, "name": "Calça", "description": "X12", "price": "399.99", "image": "calca.jpg"},
+  {"id": 6, "name": "Macacão", "description": "Deynasa", "price": "2899.99", "image": "macacao.jpg"},
+  {"id": 7, "name": "Capa de Chuva", "description": "RainProtect", "price": "75.99", "image": "capa.jpg"},
+  {"id": 8, "name": "Bala-Clava", "description": "X12", "price": "49.99", "image": "bala.jpg"}
+]
+
+export default products
