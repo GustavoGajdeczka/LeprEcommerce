ccb4761286dbc1bb33fda98468b3051b5162254f
Gustavo <tavinhogustavog@gmail.com>
2022-01-22 04:00:51 -0300

    ajuste no commerce

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,3 +1,5 @@
 import Commerce from '@chec/commerce.js'
-
+// Dev
 export const commerce = new Commerce(process.env.REACT_APP_CHEC_PUBLIC_KEY, true);
+// Build
+//export const commerce = new Commerce(process.env.REACT_APP_CHEC_PUBLIC_KEY);
