c75dfe2a52cede3c69eed02eef7f61928dc629b3
Gustavo <tavinhogustavog@gmail.com>
2022-01-22 03:59:50 -0300

    deploy 2 ajuste 4

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,3 +1,5 @@
 import Commerce from '@chec/commerce.js'
-
-export const commerce = new Commerce(process.env.REACT_APP_CHEC_PUBLIC_KEY, process.env.REACT_APP_COMMERCE_LOG);
+// Dev
+//export const commerce = new Commerce(process.env.REACT_APP_CHEC_PUBLIC_KEY, true);
+// Build
+export const commerce = new Commerce(process.env.REACT_APP_CHEC_PUBLIC_KEY);
