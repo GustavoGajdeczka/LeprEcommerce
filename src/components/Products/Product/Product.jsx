0878b61f2857e8b9ad9bd98b274dbaa75420df7d
Gustavo <tavinhogustavog@gmail.com>
2022-01-24 21:37:56 -0300

    tela de pagamento

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -17,7 +17,7 @@ const Product = ({product, onAddToCart}) => {
           <Typography variant="h5" >
             {product.price.formatted_with_symbol}
           </Typography>
-        </div>^
+        </div>
         <Typography dangerouslySetInnerHTML={{ __html: product.description}} variant="body2" color="textSecondary" />
           
       </CardContent>
